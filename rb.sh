fec01c7a8d4eebdecd0ab5a587d4a6e0520a4436
KOLAWOLE AKINTAD <kolawole_o_akintade@apple.com>
2022-02-07 19:05:32 -0600

    create s3 and s3 policy

--- rb.sh
+++
@@ -1,5 +0,0 @@
-kubectl create clusterrolebinding permissive-binding \
-  --clusterrole=cluster-admin \
-  --user=admin \
-  --user=kubelet \
-  --group=system:serviceaccounts
--- s3.sh
+++ s3.sh
@@ -2,3 +2,5 @@ aws s3api create-bucket \
     --bucket mongodbwiz \
     --region us-west-2 \
  --create-bucket-configuration LocationConstraint=us-west-2
+#---
+aws s3api put-bucket-policy --policy file://bucket-poliy --endpoint-url=https://mongodbwiz.s3.us-west-2.amazonaws.com --bucket  mongodbwiz
